ba287bbf3519dc76073a4042a5d23dde2eba080a
Guggs57 <n_guggenbuhl@hotmail.com>
2025-03-13 19:24:26 +0100

    Mise à jours le 13.03.2025

---
+++ lib/00_journalists.rb
@@ -0,0 +1,150 @@
+# Définition de l'array avec les handles
+mon_tableau = [
+    "@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle",
+    "@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard",
+    "@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap",
+    "@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures",
+    "@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry",
+    "@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa",
+    "@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou",
+    "@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron",
+    "@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg",
+    "@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF",
+    "@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi",
+    "@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau",
+    "@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal",
+    "@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever",
+    "@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital",
+    "@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita",
+    "@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas",
+    "@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy",
+    "@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx",
+    "@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy",
+    "@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy",
+    "@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo",
+    "@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc",
+    "@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie",
+    "@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas",
+    "@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri",
+    "@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi",
+    "@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens",
+    "@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams",
+    "@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien",
+    "@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic",
+    "@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard",
+    "@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene",
+    "@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann",
+    "@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi",
+    "@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari",
+    "@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert",
+    "@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro",
+    "@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt",
+    "@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua",
+    "@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe",
+    "@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune",
+    "@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio",
+    "@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon",
+    "@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon",
+    "@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"
+]
+
+# Compter le nombre d'éléments dans le tableau
+mon_tableau_compte = mon_tableau.length
+
+# Trouver le handle le plus court
+lepluspetitelem = mon_tableau.min_by { |handle| handle.length }
+
+# Afficher les résultats
+#puts "Il y a #{mon_tableau_compte} handles dans l'array."
+#puts "L'élément le plus court est : #{lepluspetitelem} avec une longueur de #{lepluspetitelem.length} caractères."
+
+
+# Compter combien de handles ont exactement 5 caractères (en excluant le @)
+compte = mon_tableau.count { |handle| handle[1..-1].length == 5 }
+
+# Afficher le résultat
+#puts "Il y a #{compte} élément élément qui contiennent 5 caractères ! "
+compte_majuscule = mon_tableau.count { |handle| handle[1].upcase == handle[1] }
+#puts "Il y a #{compte_majuscule} handles qui commencent par une majuscule."
+
+
+
+mon_tableau_trie = mon_tableau.sort
+#puts "Liste triée par ordre alphabétique :"
+#puts mon_tableau_trie
+
+mon_tableau_trie_par_taille = mon_tableau.sort_by { |handle| handle.length }
+#puts "Liste triée par taille des handles (du plus petit au plus grand) :"
+#puts mon_tableau_trie_par_taille
+
+
+position = mon_tableau.index("@epenser")
+#puts "La position de @epenser dans l'array est : #{position}"
+
+# Création du hachage pour stocker la répartition des tailles
+repartition_taille = Hash.new(0)
+
+# Parcours de chaque handle dans le tableau pour incrémenter la taille correspondante
+mon_tableau.each do |handle|
+  taille = handle.length
+  repartition_taille[taille] += 1
+end
+
+# Affichage de la répartition
+ordre_carac = repartition_taille.each do |taille, count|
+ 
+end
+
+
+
+
+question_choix = [
+" Combien y a t-il de handle dans cette array ?",
+"Quel est le handle le plus court de cette liste ? ",
+"Combien y a t'il de handl contenant 5 caractères ( sans le @)",
+" Combien commencent par une majuscule ?",
+"Trie la liste par ordre alphabétique ",
+"Trie la liste par taille",
+" Quelle est la position de @epenser ?",
+" Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc) "
+]
+
+reponses = [
+  "Il y a #{mon_tableau_compte} handle dans ce tableau.",
+  "Le Handle le plus court est #{lepluspetitelem}",
+  "Il y a #{compte} qui ont 5 caractères",
+  "Il y a #{compte_majuscule} qui commencent par une majuscule",
+  "#{mon_tableau_trie}",
+  "#{repartition_taille}",
+  "où est @epenser ?",
+  "Voici la répartition du tableau par taille de ces derniers : #{ordre_carac}",
+
+]
+
+loop do 
+  puts " quel est votre question?"
+  question_choix.each_with_index do |question_choix, index|
+       puts "#{index + 1}. #{question_choix}"
+  end
+
+print "Choisissez dans cette liste : "
+choix = gets.chomp.to_i 
+
+  if choix < 1 || choix > 8
+       puts "Numéro invalide, recommenez "
+       print "Choisissez en indiquant un numéro : "
+       choix = gets.chomp.to_i 
+  else
+       question_selec = question_choix[choix - 1]
+       reponse_selec = reponses [choix - 1]
+
+  puts " Info sélectionnée : #{question_selec}"
+  puts " réponse : #{reponse_selec}"
+
+  print " une autre information peu être ? (Oui/Non)"
+  continuer = gets.chomp.downcase
+
+  break unless continuer == "oui" 
+
+  end
+end
